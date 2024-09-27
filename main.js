2cd591ae5ccfb8ab53aedb8e3875257a5f44e895
eduardojosecep <142450672+eduardojosecep@users.noreply.github.com>
2024-09-27 09:26:42 -0300

    gh

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ function areaQuadrado(lado){
     return lado**2;
 }
 console.log (areaQuadrado(6));
+let nome=parseint(prompt('qual sua nota do 1° trimestre?'));
 let media=function (x,y,z){
     return (1+2+3)/3;
 }
 console.log(media(1+2+3/3));
-let nome=parseint(prompt('qual sua nota do 1° trimestre?'));
-console.log('qual sua nota do 1° trimestre?');
+
