7e1a4c5cf6a9b162e8c301046ff5434049717c13
eduardojosecep <142450672+eduardojosecep@users.noreply.github.com>
2024-09-27 09:15:45 -0300

    y

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function areaQuadrado(lado){
     return lado**2;
 }
 console.log (areaQuadrado(6));
-let media=function (media){
-    return (1+2+3)/3;
+let media=function (x,y,z){
+    return (x+y+z)/3;
 }
 console.log(media(1+2+3/3));
